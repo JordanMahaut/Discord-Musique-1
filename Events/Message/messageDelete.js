1a0704acae2e892c3e1758f6d80620e30c32562d
Jordan <jordanmahaut09@gmail.com>
2022-03-31 23:52:59 +0200

    Message

---
+++ Events/Message/messageDelete.js
@@ -0,0 +1,25 @@
+const { MessageEmbed, Message, WebhookClient, NewsChannel } = require("discord.js");
+
+module.exports = {
+    name: "messageDelete",
+    /**
+     * 
+     * @param {Message} message 
+     */
+    execute(message) {
+        if(message.author.bot) return;
+
+        const Log = new MessageEmbed()
+        .setColor("#36393f")
+        .setDescription(`📕 Un [message](${message.url}) par ${message.author.tag} a été **supprimer**.\n
+        **Message supprimé:**\n ${message.content ? message.content : "None"}`.slice(0, 4000))
+
+        if(message.attachments.size >= 1){
+            Log.addField(`Attachments:`, `${message.attachments.map(a => a.url)}`, true)
+        }
+
+        new WebhookClient({url: "https://discord.com/api/webhooks/959207329386082334/t8jE7xpJRMUR2H8TROC3ITKXMZfC1y1oHkl4BsvT_ip2Dn3LfkGvd7MXXkT0OkkovJvr"}
+        ).send({embeds: [Log]}).catch((err) => { console.log(err)});
+
+    }
+}
---
+++ Events/Message/messageUpdate.js
@@ -0,0 +1,27 @@
+const { MessageEmbed, Message, WebhookClient, NewsChannel } = require("discord.js");
+
+module.exports = {
+    name: "messageUpdate",
+    /**
+     * @param {Message} oldMessage 
+     * @param {Message} newMessage 
+     */
+    execute(oldMessage, newMessage) {
+        if(oldMessage.author.bot) return;
+
+        if(oldMessage.content === newMessage.content) return;
+
+        const Count = 1950;
+
+        const Original = oldMessage.content.slice(0, Count) + (oldMessage.content.length > 1950 ? " ..." : "");
+        const Edited = newMessage.content.slice(0, Count) + (newMessage.content.length > 1950 ? " ..." : "");
+
+        const log = new MessageEmbed()
+        .setColor("#36393f")
+        .setDescription(`📘 A [message](${newMessage.url}) par ${newMessage.author} **a été modifié** dans ${newMessage.channel}.\n
+        **Original**:\n ${Original} \n**Modifié**:\n ${Edited}`.slice("0", "4096"))
+
+        new WebhookClient({url: "https://discord.com/api/webhooks/959181948444614800/pVnIy8FIUFvCwMjfLHoklYClmjxHTzNmVvSX-2MYpd7q2S2Eem2gbDsxPtrPPXUBWx_B"}
+        ).send({embeds: [log]}).catch((err) => console.log(err));
+    }
+}
