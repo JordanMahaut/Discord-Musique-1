cedbf32877a1b735335bc5a38d6755ad4fef33ad
Jordan <jordanmahaut09@gmail.com>
2022-04-08 22:48:51 +0200

    Fusion

--- Commands/Systems/music.js
+++ Commands/Systems/music.js
@@ -6,29 +6,24 @@ module.exports = {
     description: "Système musical complet",
     permission: "ADMINISTRATOR",
     options: [
-        {
-            name: "play",
-            description: "Jouer une musique.",
-            type: "SUB_COMMAND",
+        { name: "play", description: "Jouer une musique.", type: "SUB_COMMAND",
             options: [{ name: "query", description: "Indiquez un nom ou une URL pour la musique", type: "STRING", required: true}]
         },
-        {
-            name: "volume",
-            description: "Changer le volume.",
-            type: "SUB_COMMAND",
+        { name: "volume", description: "Changer le volume.", type: "SUB_COMMAND",
             options: [{ name: "percent", description: "10 = 10%", type: "NUMBER", required: true}]
         },
-        {
-            name: "settings",
-            description: "Sélectionner dans les options.",
-            type: "SUB_COMMAND",
+        { name: "settings", description: "Sélectionner dans les options.", type: "SUB_COMMAND",
             options: [{ name: "options", description: "Sélectionner dans les options", type: "STRING", required: true,
             choices: [
-                {name: "queue", value: "queue"},
-                {name: "skip", value: "skip"},
-                {name: "pause", value: "pause"},
-                {name: "resume", value: "resume"},
-                {name: "stop", value: "stop"}
+                {name: "🔢 View Queue", value: "File d'attente"},
+                {name: "⏭️ Skip Song", value: "Skip"},
+                {name: "⏸️ Pause Song", value: "Pause"},
+                {name: "▶️ Resume Song", value: "Reprendre"},
+                {name: "⏹️ Stop Music", value: "Stop"},
+                {name: "🔀 Shuffle Queue", value: "Mélanger"},
+                {name: "🔃 Toggle Autoplay Modes", value: "AutoPlay"},
+                {name: "🈁 Add a Related Song", value: "Chanson associée"},
+                {name: "🔁 Toggle Repeat Mode", value: "Mode répétition"}
             ]}]
         }
     ],
@@ -69,16 +64,36 @@ module.exports = {
                     switch(options.getString("options")) {
                         case "skip" :
                         await queue.skip(VoiceChannel);
-                        return interaction.reply({content: "⏭️ La musique a été skip."})
+                        return interaction.reply({content: "⏭️ La musique a été skip."});
+
                         case "stop" :
                         await queue.stop(VoiceChannel);
-                        return interaction.reply({content: "⏭️ La musique a été stopper."})
+                        return interaction.reply({content: "⏭️ La musique a été stopper."});
+
                         case "pause" :
                         await queue.pause(VoiceChannel);
-                        return interaction.reply({content: "⏸️ La musique est en pause."})
+                        return interaction.reply({content: "⏸️ La musique est en pause."});
+
                         case "resume" :
                         await queue.resume(VoiceChannel);
-                        return interaction.reply({content: "▶️ La musique a repris."})
+                        return interaction.reply({content: "▶️ La musique a repris."});
+
+                        case "shuffle" :
+                        await queue.shuffle(VoiceChannel);
+                        return interaction.reply({content: "🔀 La file d'attente a été mélangée."});
+
+                        case "AutoPlay" :
+                        let Mode = await queue.toggleAutoplay(VoiceChannel);
+                        return interaction.reply({content: `🔃 Le mode de lecture automatique est réglé sur ${Mode ? "On" : "Off"}`});
+
+                        case "RelatedSong" :
+                        await queue.addRelatedSong(VoiceChannel);
+                        return interaction.reply({content: "🈁 Une chanson associée a été ajoutée à la file d'attente."});
+
+                        case "RepeatMode" :
+                        let Mode2 = await client.distube.setRepeatMode(queue);
+                        return interaction.reply({content: `🔃 Le mode de répétition est réglé sur ${Mode2 == Mode2 ? Mode2 == 2 ? "File d'attente" : "Chanson" : "Off"}`});
+
                         case "queue" :
                         return interaction.reply({embeds: [new MessageEmbed()
                         .setColor("PURPLE")
