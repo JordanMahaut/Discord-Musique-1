c6426b8fc212e4e77dd0c0b59c99ab1d2464cb52
Jordan <jordanmahaut09@gmail.com>
2022-04-27 22:48:10 +0200

    fusion

---
+++ Commands/Moderation/help.js
@@ -0,0 +1,7 @@
+const { MessageEmbed, Message } = require("discord.js");
+const { description } = require("./clear");
+
+module.exports = {
+    name: "help",
+    description: "Sert à voir les commandes"
+}
--- Structures/index.js
+++ Structures/index.js
@@ -1,34 +1,35 @@
 const { Client, Collection } = require("discord.js");
 const { Token } = require("./config.json");
-const client = new Client({intents: 32767});
-const { promisify } = require('util');
+const client = new Client({ intents: 32767 });
+const { promisify } = require("util");
 const { glob } = require("glob");
 const PG = promisify(glob);
 const Ascii = require("ascii-table");
 
-
-client.commands = new Collection(); 
+client.commands = new Collection();
 
 const { DisTube } = require("distube");
 const { SpotifyPlugin } = require("@distube/spotify");
 
 client.distube = new DisTube(client, {
-    emitNewSongOnly: true,
-    leaveOnFinish: true,
-    emitAddSongWhenCreatingQueue: false,
-    plugins: [new SpotifyPlugin()]
+  emitNewSongOnly: true,
+  leaveOnFinish: true,
+  emitAddSongWhenCreatingQueue: false,
+  plugins: [new SpotifyPlugin()],
 });
 module.exports = client;
- 
+
 require("../Systems/GiveawaySys")(client);
 
-["Events", "Commands"].forEach(handler => {
-    require(`./Handlers/${handler}`)(client, PG, Ascii);
-})
-
-
-client.login(Token).then(() => {
-    console.log("Client connecté en tant que " + client.user.tag)
-}).catch((err) => {
-    console.log(err)
+["Events", "Commands"].forEach((handler) => {
+  require(`./Handlers/${handler}`)(client, PG, Ascii);
 });
+
+client
+  .login(Token)
+  .then(() => {
+    console.log("Client connecté en tant que " + client.user.tag);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
